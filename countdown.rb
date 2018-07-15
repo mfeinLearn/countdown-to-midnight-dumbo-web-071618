85fa9040cc341390d02af96bf01e904f44e61f87
Malcome Fein <mfein90@gmail.com>
2018-07-15 11:08:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(number)
-  number = 1
   while number < 10
     puts "#{number} SECOND(S)!"
-    x += 1
+    number += 1
   end
   puts "HAPPY NEW YEAR!"
 end
