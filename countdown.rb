59d0b8e33611a23f5ef6b3b40a0747a786e46171
Malcome Fein <mfein90@gmail.com>
2018-07-15 11:12:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,10 +5,10 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 
 def countdown_with_sleep(number)
-  
+  sleep(0.5)
 end
